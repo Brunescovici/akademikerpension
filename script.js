a57f2397bdd3e5fae7c1fcf613edf222e7daf025
kalinastoykova <kali0173@stud.kea.dk>
2020-11-18 15:02:04 +0100

    added the live chat function

---
+++ script.js
@@ -0,0 +1,32 @@
+//functions for the 'live-chat'
+document.querySelector("#chat-icon").addEventListener("click", showChat);
+function showChat() {
+    this.classList.add("hidden");
+    document.querySelector("#chat-box").classList.remove("hidden");
+}
+
+document.querySelector(".close").addEventListener("click", hideChat);
+function hideChat() {
+    document.querySelector("#chat-icon").classList.remove("hidden");
+    document.querySelector("#chat-box").classList.add("hidden");
+}
+
+
+let chatTemplate = document.querySelector("#chat-template")
+
+sendMessageBot("Hi, my name is Lina. How may I help you today?")
+
+function sendMessageBot(message) {
+    let clone = chatTemplate.content.cloneNode(true);
+    clone.querySelector(".chat-p").textContent = message;
+    document.querySelector(".chat-content").appendChild(clone);
+}
+
+
+document.querySelector(".send-btn").addEventListener("click", sendMessageUser);
+function sendMessageUser() {
+    let clone = chatTemplate.content.cloneNode(true);
+    clone.querySelector(".chat-line").classList.add("user")
+    clone.querySelector(".chat-p").textContent = document.querySelector("#user-input").value;
+    document.querySelector(".chat-content").appendChild(clone);
+}
