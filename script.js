baf810025ba321d0a59cecf12fc35bb98be1f46c
kalinastoykova <kali0173@stud.kea.dk>
2020-11-19 01:46:59 +0100

    added styling, made a new js file for the categories

--- script.js
+++ script.js
@@ -35,17 +35,8 @@ function sendMessageUser() {
 }
 
 
-//html accordion
-function addAccordionEvents() {
-    document.querySelectorAll(".accordion").forEach(accordion => {
-        accordion.addEventListener("click", function () {
-            this.querySelector(".accordion-descr").classList.toggle("open");
-        })
-    })
-}
 
-
-//////fetching
+//fetch the data for the front page
 
 fetch("http://www.pacdesign.dk/CMS/wp-json/wp/v2/box")
     .then(res => res.json())
@@ -70,56 +61,3 @@ function addBox(data) {
 //
 
 
-//fetch data for each new category page
-
-let situations = [];
-
-//get parameters from the URL (everything after the '?' mark)
-const urlParams = new URLSearchParams(window.location.search);
-
-//get only the 'id'
-const id = urlParams.get('id');
-console.log(id);
-
-//fetch the data from wp
-function fetchData() {
-    fetch("http://www.pacdesign.dk/CMS/wp-json/wp/v2/life_change")
-        .then((resp) => resp.json())
-        .then(function (data) {
-            //to access the data outside
-            situations = data;
-            console.log(situations);
-            createTemplate(id);
-            addAccordionEvents();
-        })
-}
-
-fetchData();
-
-//create template and add stuff
-function createTemplate(postId) {
-    let heroTemplate = document.querySelector("#hero-temp");
-    let clone = heroTemplate.content.cloneNode(true);
-
-    //clone all
-    situations.forEach(function (situation) {
-        if (situation.id == postId) {
-            clone.querySelector("#category-heading").innerHTML = situation.title.rendered;
-            clone.querySelector("#hero-img-cat").src = situation.hero_image.guid;
-            clone.querySelector("#acc1 .accordion-heading h4").textContent = situation.situation_1;
-            clone.querySelector("#acc1 .accordion-descr").innerHTML = "<pre>" + situation.situation_1_descr + "</pre>";
-            clone.querySelector("#acc2 .accordion-heading h4").textContent = situation.situation_2;
-            clone.querySelector("#acc2 .accordion-descr").innerHTML = "<pre>" + situation.situation_2_descr + "</pre>";
-            document.querySelector("main").appendChild(clone);
-
-            document.querySelectorAll(".accordion").forEach(accordion => {
-                if (accordion.querySelector(".accordion-heading h4").textContent == "") {
-                    accordion.classList.add("hidden");
-                    console.log("kalak");
-                }
-                })
-            }
-        }
-    )
-
-}
